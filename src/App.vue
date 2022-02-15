17251f7359d57f8ec6ed858d6f524413efc193e2
AbdielP <abdiel.pinzonc@gmail.com>
2022-02-15 13:17:38 -0500

    navigation component created

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="main-container">
+  <div>
+    <Navigation v-if="$route.name !== 'Login'" />
     <router-view />
   </div>
 </template>
 
+<script>
+import Navigation from "@/components/Navigation.vue";
+export default {
+  components: {
+    Navigation,
+  },
+};
+</script>
+
 <style lang="scss">
 * {
   box-sizing: border-box;
+  padding: 0;
   margin: 0;
 }
 
---
+++ src/components/Navigation.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav class="nav">
+    <router-link class="nav__link" to="/auth/me">User</router-link>
+    <router-link class="nav__link" v-if="isLoggedIn === false" to="/auth/login">Login</router-link>
+    <button
+      type="button"
+      v-if="isLoggedIn === true"
+      @click="logOut"
+      aria-labelby="Logout">
+      Logout
+    </button>
+    <!-- {{isLoggedIn}}  {{getToken}} -->
+  </nav>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters(["isLoggedIn", "getToken"]),
+  },
+  methods: {
+    logOut() {
+      localStorage.removeItem("jwtapptoken");
+      this.$router.push("/auth/login");
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.nav
+    background: #ccc
+    padding: 5px
+
+    .nav__link
+        text-decoration: none
+        margin: 0 5px
+        color: #fff
+        &:hover
+            color: #D8D8D8
+</style>

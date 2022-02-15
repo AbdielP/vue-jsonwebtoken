8671f6df1dab64211b44693d6587a3fab3d22b1b
AbdielP <abdiel.pinzonc@gmail.com>
2022-02-15 13:07:40 -0500

    user info page and token validation

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,12 +1,54 @@
 <template>
   <div>
-    <h1>About me!</h1>
-    Ahora debes redireccionar a login si el token expira
+    <h1>User info</h1>
+    <label
+      >User data
+      <hr />
+      <ul v-if="userinfo">
+        <li>First name:</li>
+        <li>Lastname name:</li>
+        <li>Username: {{ userinfo.username }}</li>
+        <li>Email: {{ userinfo.email }}</li>
+      </ul>
+    </label>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
   name: "User",
+  data() {
+    return {
+      userinfo: "",
+    };
+  },
+  computed: {
+    ...mapGetters(["getToken"]),
+  },
+  methods: {
+    async getUserInfo() {
+      try {
+        const response = await fetch(`https://backend-node-server.herokuapp.com/api/vueforms/user/info?token=${this.getToken}`, {
+        // const response = await fetch(`http://localhost:3000/api/vueforms/user/info?token=${this.getToken}`,{
+            method: "GET",
+          }
+        );
+        const data = await response.json();
+        !data.ok ? this.errorHandler(data.err) : this.userinfo = data.user;;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    errorHandler(err) {
+      if (err.name === "TokenExpiredError") {
+        localStorage.removeItem("jwtapptoken");
+        return this.$router.push("/auth/login");
+      }
+    },
+  },
+  created() {
+    this.getUserInfo();
+  },
 };
 </script>

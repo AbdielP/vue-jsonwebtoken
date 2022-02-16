92598f7ab4ebad84353d7c960f431497b87fbceb
AbdielP <abdiel.pinzonc@gmail.com>
2022-02-16 13:52:11 -0500

    baseInput component created

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- <label>{{label}}</label> -->
+  <input :type="type" :placeholder="label" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" />
+</template>
+
+<script>
+export default {
+    props: {
+        type: {
+            type: String,
+            default: 'Text'
+        },
+        label: {
+            type: String,
+            dafault: ''
+        },
+        modelValue: {
+            type: [String, Number],
+            default: ''
+        }
+    }
+}
+</script>
--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -3,45 +3,40 @@
     <h1>Create account!</h1>
     <section class="section__form">
       <form @submit.prevent="signIn" class="form display-flex">
-        <input v-model="firstname"
-          type="text"
-          placeholder="First name"
-          aria-labelledby="First name"
-        />
-        <input v-model="lastname"
-          type="text"
-          placeholder="Last name"
-          aria-labelledby="Last name"
-        />
-        <input v-model="username" type="text" placeholder="Username" aria-labelledby="Username" />
-        <input v-model="password"
-          type="password"
-          placeholder="Password"
-          aria-labelledby="Password"
-        />
-        <!-- Email -->
-        <input v-model="email" type="email" placeholder="Email" aria-labelledby="Email" />
+        <BaseInput v-model="firstname" label="First Name"/>
+        <BaseInput v-model="lastname" label="Last Name"/>
+        <BaseInput v-model="username" label="Username"/>
+        <BaseInput type="password" v-model="password" label="Password"/>
+        <BaseInput type="email" v-model="email" label="Email Address"/>
         <!-- Contact number -->
         <div class="display-flex">
-            <select v-model="type">
-                <option value="" disabled selected>-</option>
-                <option value="mobile">Mobile</option>
-                <option value="phone">Phone</option>
-            </select>
-           <input v-model="number" type="text" placeholder="Contact number" aria-labelledby="Contact number" />
+          <select v-model="type">
+            <option value="" disabled selected>-</option>
+            <option value="mobile">Mobile</option>
+            <option value="phone">Phone</option>
+          </select>
+          <BaseInput v-model="number" label="Contact number"/>
         </div>
         <!-- Extra contact number -->
         <div v-for="(row, index) in rows" :key="index" class="display-flex">
-            <select>
-                <option value="" disabled selected>-</option>
-                <option value="mobile">Mobile</option>
-                <option value="phone">Phone</option>
-            </select>
-           <input type="text" placeholder="Contact number" aria-labelledby="Contact number" />
-           <button @click="removeRow(index)" type="button" aria-labelledby="Remove number">Remove -</button>
+          <select v-model="row.type">
+            <option value="" disabled selected>-</option>
+            <option value="mobile">Mobile</option>
+            <option value="phone">Phone</option>
+          </select>
+          <BaseInput v-model="row.number" label="Contact number"/>
+          <button
+            @click="removeRow(index)"
+            type="button"
+            aria-labelledby="Remove number"
+          >
+            Remove -
+          </button>
         </div>
-        <button @click="addRow" type="button" aria-labelledby="Add phone +">Add phone +</button>
-        <br>
+        <button @click="addRow" type="button" aria-labelledby="Add phone +">
+          Add phone +
+        </button>
+        <br />
         <button type="submit">Sign in</button>
       </form>
     </section>
@@ -49,44 +44,72 @@
 </template>
 
 <script>
+import BaseInput from '@/components/BaseInput.vue';
 export default {
   name: "Signin",
+  components: {
+    BaseInput
+  },
   data() {
     return {
       firstname: "",
       lastname: "",
       username: "",
       password: "",
+      number: "",
       email: "",
       type: "",
-      number: "",
-      rows: []
+      rows: [],
     };
   },
   methods: {
-    signIn() {
-      console.log(this.rows);
+    async signIn() {
+      try {
+        const response = await fetch(
+          // `https://backend-node-server.herokuapp.com/api/vueforms/signin`,
+          `http://localhost:3000/api/vueforms/signin`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              firstname: this.firstname,
+              lastname: this.lastname,
+              username: this.username,
+              password: this.password,
+              number: this.number,
+              email: this.email,
+              type: this.type,
+              rows: this.rows,
+            }),
+          }
+        );
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {}
     },
     addRow() {
-        this.rows.push([{
-            type: "",
-            number: ""
-        }])
+      this.rows.push({
+        type: "",
+        number: "",
+      });
+      console.log(this.rows);
     },
-    removeRow (index) {
-        this.rows.splice(index, 1)
-    }
+    removeRow(index) {
+      this.rows.splice(index, 1);
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
 h1 {
-    text-align: center;
+  text-align: center;
 }
 .section__form {
-    margin: auto;
-    width: 50%;
+  margin: auto;
+  width: 50%;
   .form {
     flex-direction: column;
   }
